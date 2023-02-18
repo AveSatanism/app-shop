4c68ff86869fbd6e664bfb6a2c35e4cd4321784c
AveSatanism <yanalfast@gmail.com>
2023-02-18 15:13:46 +0300

    feat: add search and KACHALKA title

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="app_flex">
+    <h1 class="site_title">KACHALKA</h1>
+    <v-text-field v-model="serch" clearable label="Поиск" variant="solo"></v-text-field>
     <v-container class="app_container">
-      <appCard>Lodash 
+      <appCard v-if="searchName.includes('lodash')">Lodash 
         <template #subtitle>A modern JavaScript utility library<br> delivering modularity,<br> performance & extras.</template>
         <template #button>      
           <v-btn
@@ -13,7 +15,7 @@
           </v-btn></template>
         <template #image><v-img src="https://lodash.com/assets/img/lodash.svg"></v-img></template>
       </appCard>
-      <appCard>Chalk
+      <appCard v-if="searchName.includes('chalk')">Chalk
         <template #subtitle>Terminal string styling done right</template>
         <template #button>      
           <v-btn
@@ -25,7 +27,55 @@
           </v-btn></template>
         <template #image><v-img src="https://raw.githubusercontent.com/chalk/chalk/HEAD/media/logo.svg"></v-img></template>
       </appCard>
-      <appCard>Quasar
+      <appCard v-if="searchName.includes('quasar')">Quasar
+        <template #subtitle>Build high-performance VueJS<br> user interfaces in<br> record time.</template>
+        <template #button>      
+          <v-btn
+            variant="outlined"
+            size="small"
+            @click="install('quasar')"
+          >
+            start
+          </v-btn></template>
+        <template #image><v-img src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"></v-img></template>
+      </appCard>
+      <appCard v-if="searchName.includes('quasar')">Quasar
+        <template #subtitle>Build high-performance VueJS<br> user interfaces in<br> record time.</template>
+        <template #button>      
+          <v-btn
+            variant="outlined"
+            size="small"
+            @click="install('quasar')"
+          >
+            start
+          </v-btn></template>
+        <template #image><v-img src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"></v-img></template>
+      </appCard>
+      <appCard v-if="searchName.includes('quasar')">Quasar
+        <template #subtitle>Build high-performance VueJS<br> user interfaces in<br> record time.</template>
+        <template #button>      
+          <v-btn
+            variant="outlined"
+            size="small"
+            @click="install('quasar')"
+          >
+            start
+          </v-btn></template>
+        <template #image><v-img src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"></v-img></template>
+      </appCard>
+      <appCard v-if="searchName.includes('quasar')">Quasar
+        <template #subtitle>Build high-performance VueJS<br> user interfaces in<br> record time.</template>
+        <template #button>      
+          <v-btn
+            variant="outlined"
+            size="small"
+            @click="install('quasar')"
+          >
+            start
+          </v-btn></template>
+        <template #image><v-img src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"></v-img></template>
+      </appCard>
+      <appCard v-if="searchName.includes('quasar')">Quasar
         <template #subtitle>Build high-performance VueJS<br> user interfaces in<br> record time.</template>
         <template #button>      
           <v-btn
@@ -50,8 +100,17 @@ export default {
   },
   data() {
    return {
+    serch: '',
+    nameData: ['lodash', 'chalk', 'quasar'],
     appName: ''
    }
+  },
+  computed: {
+    searchName() {
+      return this.nameData.filter( elem => {
+        return elem.includes(this.serch);
+      });
+    },
   },
   methods: {
     install(appName) {
@@ -66,7 +125,13 @@ export default {
 <style>
 .app_flex {
   display: flex;
+  row-gap: 50px;
+  min-height: 100vh;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  padding: 30px 50px;
+  background-color: #08E8DE;
 }
 .app_container {
   display: flex;
@@ -75,8 +140,21 @@ export default {
   max-width: 1200px;
   gap: 20px;
 }
+.site_title {
+  font-size: 120px;
+}
 .v-card-actions {
   padding: 8px 16px !important;
+}
+.v-input__details {
+  display: none !important;
+}
+.v-input{
+  flex: none !important;
+}
+.v-input__control{
+  height: 55px;
+  min-width: 500px;
 }
  button {
   height: 20px;
